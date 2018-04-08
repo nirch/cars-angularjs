8406be2f71876285e442147fe4a47257eb734966
nirch <nirh2@yahoo.com>
2018-04-08 09:39:54 +0300

    Added login route + basic html

--- app/app.js
+++ app/app.js
@@ -6,4 +6,7 @@ app.config(function($routeProvider) {
     .when("/", {
       templateUrl : "app/home/home.html"
     })
+    .when("/login", {
+      templateUrl : "app/login/login.html"
+    })
 });
