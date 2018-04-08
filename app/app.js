85bb15050f5e8940e6723c5554e9bc4333d6bcb9
nirch <nirh2@yahoo.com>
2018-04-08 09:31:29 +0300

    Added routing + simple home page

--- app/app.js
+++ app/app.js
@@ -1,2 +1,9 @@
 
-var app = angular.module('carApp', []);
+var app = angular.module('carApp', ['ngRoute']);
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+      templateUrl : "app/home/home.html"
+    })
+});
