f6c7beec3416df92c6b46766f11e9af795a925bc
nirch <nirh2@yahoo.com>
2018-04-08 10:50:21 +0300

    loginCtrl has login function (without navigation)

--- app/login/loginCtrl.js
+++ app/login/loginCtrl.js
@@ -1,4 +1,19 @@
 
 app.controller('loginCtrl', function($scope, activeUserService, $log) {
-    $scope.test = "blabla"
+
+    $scope.invalidCredentails = false;
+
+    $scope.login = function() {
+        // TODO: Here you should disable the login button until there is a response from the service
+
+        activeUserService.load().then(function() {
+            var successLogin = activeUserService.login($scope.email, $scope.pwd);
+            if (successLogin) {
+                
+            } else {
+                // TODO: Missing hadleing of next try
+                $scope.invalidCredentails = true;
+            }
+        })
+    }
 });
