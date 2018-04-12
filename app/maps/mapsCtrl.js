077598d4068fe53b5b87efb58aeb8b777ebc5d4e
nirch <nirh2@yahoo.com>
2018-04-12 10:34:09 +0300

    added google maps with $sce (trustAsResourceUrl)

--- app/maps/mapsCtrl.js
+++ app/maps/mapsCtrl.js
@@ -1,3 +1,5 @@
-app.controller('mapsCtrl', function($scope, carService) {
-    $scope.test = "blabla";
+app.controller('mapsCtrl', function($scope, carService, $sce) {
+    var mapSearch = "TelAviv"
+    $scope.mapURL = $sce.trustAsResourceUrl("https://www.google.com/maps/embed/v1/search?key=AIzaSyBSil-hbUge7NwSpdgLL6zHWNgJfQzlHcs&q=" + mapSearch);
+    
 });
